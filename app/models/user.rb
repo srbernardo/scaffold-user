b77bd68279916f9cec3566c890afda49847899fe
bernardo brotelli <b.brotelle@gmail.com>
2023-10-07 14:47:44 -0300

    Setup pg_search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  include PgSearch::Model
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   phone_regex = /\A\(\d{2}\)\s\d{4,5}-\d{4}\z/
   cpf_regex = /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/
+
+  pg_search_scope :search_by_all_fields,
+    against: [ :name, :email, :phone, :cpf ],
+    using: {
+      tsearch: { prefix: true }
+    }
 
   validates :email, presence: true, format: { with: email_regex}
   validates :cpf, presence: true, format: { with: cpf_regex, message: "is invalid, Ex: 123.456.789-00" }
