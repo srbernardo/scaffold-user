0364b6ce6f7d81683e714989f58d0267d1ab8058
bernardo brotelli <b.brotelle@gmail.com>
2023-10-10 10:04:49 -0300

    Add console log of connect and add action of form to url

--- app/javascript/controllers/instant_search_controller.js
+++ app/javascript/controllers/instant_search_controller.js
@@ -2,10 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="instant-search"
 export default class extends Controller {
-  static targets = ["input", "results"]
+  static targets = ["input", "results", "form"]
+
+  connect() {
+    console.log("Searching...");
+  }
 
   update() {
-    const url = `/?query=${this.inputTarget.value}`
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then(data => {
