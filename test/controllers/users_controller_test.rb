304846e504b8fb93b4c00245b58beb1cf22471b6
bernardo brotelli <b.brotelle@gmail.com>
2023-10-11 16:21:05 -0300

    Add new tests for user_controller and user_model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,123 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
+  def setup
+    User.destroy_all
+    @user = User.create(name: "Jose", email: "jose@email.com", phone: "(82) 91177-6655", cpf: "123.456.789-11")
   end
 
-  test "should get index" do
+  test "assigns all users to @users if query is empty or nil" do
+    user2 = User.create(name: "Maria", email: "maria@email.com", phone: "(82) 99977-6655", cpf: "123.456.789-00")
+
+    get users_url, params: { query: "" }
+    assert_equal [@user, user2], assigns(:users)
+  end
+
+  test "assigns users matching the query to @users" do
+    get users_url, params: { query: "Jose" }
+
+    assert_equal [@user], assigns(:users)
+  end
+
+  test "renders the index template" do
     get users_url
+
     assert_response :success
+    assert_template :index
   end
 
-  test "should get new" do
-    get new_user_url
+  test "renders the users list partial as text" do
+    get users_url, as: :text
+
     assert_response :success
+    assert_template partial: "users/_list"
   end
 
-  test "should create user" do
-    assert_difference("User.count") do
-      post users_url, params: { user: { cpf: @user.cpf, email: @user.email, name: @user.name, phone: @user.phone } }
-    end
-
-    assert_redirected_to user_url(User.last)
-  end
-
-  test "should show user" do
+  test "assigns the requested user to @user" do
     get user_url(@user)
-    assert_response :success
+
+    assert_equal @user, assigns(:user)
   end
 
-  test "should get edit" do
+  test "renders the show template" do
+    get user_url(@user)
+
+    assert_response :success
+    assert_template :show
+  end
+
+  test "assigns a new user to @user" do
+    get new_user_url
+
+    assert_instance_of User, assigns(:user)
+    assert assigns(:user).new_record?
+  end
+
+  test "renders the new template" do
+    get new_user_url
+
+    assert_response :success
+    assert_template :new
+  end
+
+  test "assigns the requested edit user to @user" do
     get edit_user_url(@user)
+
+    assert_equal @user, assigns(:user)
+  end
+
+  test "renders the edit template" do
+    get edit_user_url(@user)
+
     assert_response :success
+    assert_template :edit
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { cpf: @user.cpf, email: @user.email, name: @user.name, phone: @user.phone } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user)
+  test "creates a new user with valid parameters" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { name: "Antonio", email: "antonio@email.com", phone: "(82) 98877-6655", cpf: "123.456.789-00"} }
     end
+    assert_redirected_to user_url(User.last)
+    assert_equal "User was successfully created.", flash[:notice]
+  end
 
+  test "does not create a new user with invalid parameters" do
+    assert_no_difference("User.count") do
+      post users_url, params: { user: {name: "Jose", email: "", phone: "", cpf: ""} }
+    end
+    assert_template :new
+    assert_response :unprocessable_entity
+    assert_includes response.body, "Please review the problems below:"
+  end
+
+  test "updates the requested user with valid parameters" do
+    user = User.create(name: "Antonio", email: "antonio@email.com", phone: "(82) 98877-6655", cpf: "123.456.789-00")
+    patch user_url(user), params: { user: { name: "Maria" } }
+
+    user.reload
+    assert_equal "Maria", user.name
+    assert_redirected_to user_url(user)
+    assert_equal "User was successfully updated.", flash[:notice]
+  end
+
+  test "does not update the requested user with invalid parameters" do
+    user = User.create(name: "Antonio", email: "antonio@email.com", phone: "(82) 98877-6655", cpf: "123.456.789-00")
+    patch user_url(user), params: { user: { email: "" } }
+
+    user.reload
+    assert_equal "antonio@email.com", user.email
+    assert_template :edit
+    assert_response :unprocessable_entity
+    assert_includes response.body, "Please review the problems below:"
+  end
+
+  test "destroys the requested user" do
+    user = User.create(name: "Antonio", email: "antonio@email.com", phone: "(82) 98877-6655", cpf: "123.456.789-00")
+
+    assert_difference("User.count", -1) do
+      delete user_url(user)
+    end
     assert_redirected_to users_url
+    assert_equal "User was successfully destroyed.", flash[:notice]
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,79 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Jose", email: "jose@email.com", cpf: "123.456.789-00", phone: "(00) 0000-0000")
+  end
+
+  test "valid user with valid attributes" do
+    assert @user.valid?
+  end
+
+  test "invalid without a name" do
+    @user.name = nil
+    assert_not @user.valid?
+  end
+
+  test "invalid without an email" do
+    @user.email = nil
+    assert_not @user.valid?
+  end
+
+  test "invalid with an invalid email format" do
+    @user.email = "invalid_email"
+    assert_not @user.valid?
+  end
+
+  test "invalid with a duplicate email" do
+    User.create(name: "Maria", email: "jose@email.com", cpf: "987.654.321-00", phone: "(11) 1111-1111")
+    assert_not @user.valid?
+  end
+
+  test "invalid without a cpf" do
+    @user.cpf = nil
+    assert_not @user.valid?
+  end
+
+  test "invalid with an invalid cpf format" do
+    @user.cpf = "12345678900"
+    assert_not @user.valid?
+  end
+
+  test "invalid with a duplicate cpf" do
+    User.create(name: "Maria", email: "maria@email.com", cpf: "123.456.789-00", phone: "(11) 1111-1111")
+    assert_not @user.valid?
+  end
+
+  test "invalid without a phone" do
+    @user.phone = nil
+    assert_not @user.valid?
+  end
+
+  test "invalid with an invalid phone format" do
+    @user.phone = "1234567890"
+    assert_not @user.valid?
+  end
+
+  test "invalid with a duplicate phone" do
+    User.create(name: "Maria", email: "maria@email.com", cpf: "987.654.321-00", phone: "(00) 0000-0000")
+    assert_not @user.valid?
+  end
+
+  test "search_by_all_fields returns matching users" do
+    user = User.create(name: "Jose", email: "jose@email.com", cpf: "123.456.789-00", phone: "(00) 0000-0000")
+
+    assert_includes User.search_by_all_fields("Jose"), user
+    assert_includes User.search_by_all_fields("jose@email.com"), user
+    assert_includes User.search_by_all_fields("(00) 0000-0000"), user
+    assert_includes User.search_by_all_fields("123.456.789-00"), user
+  end
+
+  test "search_by_all_fields does not return non-matching users" do
+    user = User.create(name: "Jose", email: "jose@email.com", cpf: "123.456.789-00", phone: "(00) 0000-0000")
+
+    assert_not_includes User.search_by_all_fields("NonExistent"), user
+    assert_not_includes User.search_by_all_fields("nonexistent@example.com"), user
+    assert_not_includes User.search_by_all_fields("(99) 9999-9999"), user
+    assert_not_includes User.search_by_all_fields("98765432100"), user
+  end
 end
