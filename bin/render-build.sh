5d218d8d2b6a5f4ea0d75241c5623eec707656e4
Bernardo Brotelli <73710552+srbernardo@users.noreply.github.com>
2024-04-24 13:36:56 -0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,8 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rails generate devise:install
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
 
