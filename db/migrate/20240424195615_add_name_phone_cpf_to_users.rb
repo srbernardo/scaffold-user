3eedc31830e2f543669cbcfd97620078bf43ac63
bernardo-brotelli <b.brotelle@gmail.com>
2024-04-24 16:58:35 -0300

    Add AddNamePhoneCpfToUsers migration

---
+++ db/migrate/20240424195615_add_name_phone_cpf_to_users.rb
@@ -0,0 +1,7 @@
+class AddNamePhoneCpfToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone, :string
+    add_column :users, :cpf, :string
+  end
+end
